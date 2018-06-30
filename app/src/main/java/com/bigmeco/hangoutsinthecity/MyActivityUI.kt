dfaaa66876b2439c439023f6271a760cbc732b6d
bigmeco <bigmecoprog@gmail.com>
2018-06-30 23:38:01 +0300

    test anko

--- app/src/main/java/com/bigmeco/hangoutsinthecity/MyActivityUI.kt
+++ app/src/main/java/com/bigmeco/hangoutsinthecity/MyActivityUI.kt
@@ -1,15 +1,25 @@
 package com.bigmeco.hangoutsinthecity
 
+import android.widget.LinearLayout
 import org.jetbrains.anko.*
 import org.jetbrains.anko.sdk25.coroutines.onClick
 
 class MyActivityUI : AnkoComponent<MainActivity> {
     override fun createView(ui: AnkoContext<MainActivity>) = ui.apply {
-        verticalLayout {
-            val name = editText()
-            button("Say Hello") {
-                onClick { ctx.toast("Hello, ${name.text}!") }
+        scrollView {
+            verticalLayout {
+                for (i in 1..50) {
+                    println(i)
+
+                    textView(i.toString() + " ") {
+                        id = Ids.textView
+                    }.lparams(width = wrapContent, height = wrapContent)
+                }
             }
         }
     }.view
+
+    private object Ids {
+        val textView = 1
+    }
 }
