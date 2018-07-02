f4761dd5a38dfdd82c31b9614a69a301cc4fbd8a
bigmeco <bigmecoprog@gmail.com>
2018-07-02 18:51:51 +0300

    fragment user add

--- app/src/main/java/com/bigmeco/hangoutsinthecity/MainActivity.kt
+++ app/src/main/java/com/bigmeco/hangoutsinthecity/MainActivity.kt
@@ -3,17 +3,33 @@ package com.bigmeco.hangoutsinthecity
 import android.app.Fragment
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.Toast
+
+
 
 
 class MainActivity : AppCompatActivity() {
 
 
 
+
+    fun  onClickChat(view: View) {
+    }
+    fun  onClickUser(view: View) {
+        transitionFragment(UserFragment())
+    }
+
+    fun  onClickAddChat(view: View) {
+        transitionFragment(MapsFragment())
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //transitionFragment(MapsFragment())
+        transitionFragment(MapsFragment())
 
 
     }
@@ -24,6 +40,7 @@ class MainActivity : AppCompatActivity() {
         ft.commit()
 
     }
+
 }
 
 
