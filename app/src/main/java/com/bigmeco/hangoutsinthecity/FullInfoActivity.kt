2dc8c1fb3e56bb5e1f4fa26ef03f4633d25f92e0
bigmeco <bigmecoprog@gmail.com>
2018-07-04 13:13:17 +0300

    add beck icon

--- app/src/main/java/com/bigmeco/hangoutsinthecity/FullInfoActivity.kt
+++ app/src/main/java/com/bigmeco/hangoutsinthecity/FullInfoActivity.kt
@@ -2,11 +2,69 @@ package com.bigmeco.hangoutsinthecity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.constraint.ConstraintSet
+import android.support.transition.TransitionManager
+import android.support.v4.content.ContextCompat
+import android.util.Log
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_full_info.*
 
 class FullInfoActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_full_info)
+
+
+    }
+
+
+    fun onClickChat(view: View) {
+        val set = ConstraintSet()
+        removeTabColor(buttonChat)
+
+
+        set.clone(parentButtonView)
+        onReportLayout(set)
+        TransitionManager.beginDelayedTransition(parentButtonView)
+        set.applyTo(parentButtonView)
+    }
+
+    fun onClickInfo(view: View) {
+        val set = ConstraintSet()
+        removeTabColor(buttonInfo)
+        set.clone(parentButtonView)
+        onListLayout(set)
+        TransitionManager.beginDelayedTransition(parentButtonView)
+        set.applyTo(parentButtonView)
+    }
+
+
+
+    private fun onListLayout(set: ConstraintSet) {
+        set.clear(R.id.dividerHorizontal, ConstraintSet.START)
+        set.connect(R.id.dividerHorizontal, ConstraintSet.START, R.id.dividerVertical, ConstraintSet.END)
+        set.clear(R.id.dividerHorizontal, ConstraintSet.END)
+        set.connect(R.id.dividerHorizontal, ConstraintSet.END, ConstraintSet.PARENT_ID, ConstraintSet.END)
+
+
+    }
+
+    private fun onReportLayout(set: ConstraintSet) {
+        set.clear(R.id.dividerHorizontal, ConstraintSet.START)
+        set.connect(R.id.dividerHorizontal, ConstraintSet.START, ConstraintSet.PARENT_ID, ConstraintSet.START)
+        set.clear(R.id.dividerHorizontal, ConstraintSet.END)
+        set.connect(R.id.dividerHorizontal, ConstraintSet.END, R.id.dividerVertical, ConstraintSet.START)
+    }
+
+
+    private fun removeTabColor(itam: TextView) {
+        buttonChat.setTextColor(resources.getColor(R.color.cardview_dark_background))
+        buttonInfo.setTextColor(resources.getColor(R.color.cardview_dark_background))
+        itam.setTextColor(resources.getColor(R.color.colorAccent))
+
+
     }
 }
